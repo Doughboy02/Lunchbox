36b700f989ee29d6c2dbc51443bda05a61ba5fca
Phyrol <nitehawkk123@gmail.com>
2021-01-28 02:00:02 -0700

    Lighting
    - Lighthouse light
    - Skybox light setup
    - Spin script for light

---
+++ Assets/Scripts/SpinLight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinLight : MonoBehaviour
+{
+    public float spinSpeed = 0.37f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up * spinSpeed);
+    }
+}
