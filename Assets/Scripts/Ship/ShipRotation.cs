3b9b24ec3f7647d009b1f9b7fb6b15373ea6ee4e
propredator23 <christiandash@hotmail.com>
2021-01-29 03:49:36 -0700

    Basic Forward  Rotation
    Added a satisfying rotation for the ship according to the waves for the pitch

---
+++ Assets/Scripts/Ship/ShipRotation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipRotation : MonoBehaviour
+{
+    public float zOffset;
+    public float xOffset;
+    public float yOffset;
+    public float maxPitchAngle;
+    public float pitchCorrectionRate;
+    public float rollRange;
+    public float rollCorrectionRate;
+    public Terrain terrain;
+    private float front,center,back;
+    private float left, right;
+
+    Vector3 frontPosition, centerPosition, backPosition,leftPosition,rightPosition;
+
+    private void Update()
+    {
+        frontPosition = transform.position + transform.forward.normalized * zOffset;
+        centerPosition = transform.position;
+        backPosition = transform.position - transform.forward.normalized * zOffset;
+
+        leftPosition = transform.position - transform.right.normalized * xOffset;
+        rightPosition = transform.position + transform.right.normalized * xOffset;
+
+        front = Terrain.activeTerrain.SampleHeight(frontPosition);
+        center = Terrain.activeTerrain.SampleHeight(centerPosition);
+        back = Terrain.activeTerrain.SampleHeight(backPosition);
+        left = Terrain.activeTerrain.SampleHeight(leftPosition);
+        right = Terrain.activeTerrain.SampleHeight(rightPosition);
+
+        transform.position = new Vector3(transform.position.x, center - yOffset, transform.position.z);
+        Vector3 directionZ = backPosition - frontPosition;
+        Vector3 directionX = rightPosition - leftPosition;
+        float checkZ = Vector3.Angle(Vector3.up,directionZ);
+        float checkX = Vector3.Angle(Vector3.up,directionX);
+        //print(checkZ);
+        //print(checkX);
+        if (checkZ > maxPitchAngle) transform.Rotate(transform.right.normalized * pitchCorrectionRate);
+        if (checkX < 90 - rollRange) transform.Rotate(transform.forward.normalized * -rollCorrectionRate);
+        if (checkX > 90 + rollRange) transform.Rotate(transform.forward.normalized * rollCorrectionRate);
+        transform.Rotate(transform.right.normalized * (back - front));
+        //transform.Rotate(transform.forward.normalized * (left - right));
+    }
+}
