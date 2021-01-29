c3b2db2444088a22736f2216f22e2fda87d7812b
Phyrol <nitehawkk123@gmail.com>
2021-01-29 00:00:18 -0700

    End Criteria Script Setup
    literally 2 lines of code

---
+++ Assets/Scripts/Dock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dock : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            //do end game stuff here
+            Destroy(collision.gameObject);
+        }
+    }
+}
