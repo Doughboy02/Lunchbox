2a2be2a1b201a60764aeea6d9983d038c07e9d7d
Phyrol <nitehawkk123@gmail.com>
2021-01-29 00:36:27 -0700

    audio intial setup maybe

---
+++ Assets/Scripts/Audio/VolumeSlider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    public Slider slider;
+
+    AudioManager audioStuff;
+
+    private void Awake()
+    {
+        audioStuff = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioManager>();
+
+        slider.onValueChanged.AddListener(delegate { adjustVolume(audioStuff); });
+
+        slider.value = audioStuff.GetVolume();
+    }
+
+    void adjustVolume(AudioManager manager)
+    {
+        manager.onVolumeChange(slider.value);
+    }
+}
