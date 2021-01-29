a99156b4291754c422e26de22051810c5205f1f1
Phyrol <nitehawkk123@gmail.com>
2021-01-29 01:18:15 -0700

    rock damage
    - rock hurts ship

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField]
+    private float damageToDeal = 20f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            ShipHealth.instance.Damage(damageToDeal);
+        }
+    }
+}
