ad855107e2e5b2c0b8409430b329208612b786b1
propredator23 <christiandash@hotmail.com>
2021-01-30 02:06:35 -0700

    Fully Functional Ship Rotation
    -The rotation of the ship is now fully functional based on the waves

--- Assets/Scripts/Ship/ShipRotation.cs
+++ Assets/Scripts/Ship/ShipRotation.cs
@@ -7,12 +7,9 @@ public class ShipRotation : MonoBehaviour
     public float zOffset;
     public float xOffset;
     public float yOffset;
-    public float maxPitchRange;
-    public float pitchCorrectionRate;
-    public float rollRange;
-    public float rollCorrectionRate;
+
     public Terrain terrain;
-    private float front,center,back;
+    public float front,center,back;
     private float left, right;
 
     Vector3 frontPosition, centerPosition, backPosition,leftPosition,rightPosition;
@@ -32,18 +29,24 @@ public class ShipRotation : MonoBehaviour
         left = Terrain.activeTerrain.SampleHeight(leftPosition);
         right = Terrain.activeTerrain.SampleHeight(rightPosition);
 
+        backPosition = new Vector3(backPosition.x,back,backPosition.z);
+        frontPosition = new Vector3(frontPosition.x,front,frontPosition.z);
+        leftPosition = new Vector3(leftPosition.x, left, leftPosition.z);
+        rightPosition = new Vector3(rightPosition.x, right, rightPosition.z);
+
         transform.position = new Vector3(transform.position.x, center - yOffset, transform.position.z);
-        Vector3 directionZ = backPosition - frontPosition;
+        Vector3 directionZ = frontPosition - backPosition;
         Vector3 directionX = rightPosition - leftPosition;
         float checkZ = Vector3.Angle(Vector3.up,directionZ);
         float checkX = Vector3.Angle(Vector3.up,directionX);
         //print(checkZ);
         //print(checkX);
-        if (checkZ > 90 + maxPitchRange) transform.Rotate(transform.right.normalized * pitchCorrectionRate);
-        if (checkZ < 90 - maxPitchRange) transform.Rotate(transform.right.normalized * -pitchCorrectionRate);
-        if (checkX < 90 - rollRange) transform.Rotate(transform.forward.normalized * -rollCorrectionRate);
-        if (checkX > 90 + rollRange) transform.Rotate(transform.forward.normalized * rollCorrectionRate);
-        transform.Rotate(transform.right.normalized * (back - front));
-        transform.Rotate(transform.forward.normalized * (left - right));
+        //if (checkZ > 90 + maxPitchRange) transform.Rotate(transform.right.normalized * pitchCorrectionRate);
+        //if (checkZ < 90 - maxPitchRange) transform.Rotate(transform.right.normalized * -pitchCorrectionRate);
+        //if (checkX < 90 - rollRange) transform.Rotate(transform.forward.normalized * -rollCorrectionRate);
+        //if (checkX > 90 + rollRange) transform.Rotate(transform.forward.normalized * rollCorrectionRate);
+        transform.rotation = Quaternion.Euler(-90 + checkZ,transform.rotation.eulerAngles.y, -90 + checkX);
+        //transform.Rotate(transform.right.normalized * (back - front));
+        //transform.Rotate(transform.forward.normalized * (left - right));
     }
 }
