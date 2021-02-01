597c2ad8a702cbb1842d117405c5f0dbfa29c494
propredator23 <christiandash@hotmail.com>
2021-02-01 01:20:39 -0700

    Improved Movement
    -Updated the ShipMovement script to feel less like ice skating and more like an actual boat

--- Assets/Scripts/Ship/ShipMovement.cs
+++ Assets/Scripts/Ship/ShipMovement.cs
@@ -9,10 +9,17 @@ public class ShipMovement : MonoBehaviour
 
     public float x, z;
 
+    public float decceleration;
+    public float rotationDecceleration;
+    public float velocity;
     public float acceleration;
+    private float _acceleration;
     public float rotationRate;
+    private float _rotationRate;
     public float maxRotation;
-    public float maxVelocity;
+
+    bool rotAccelerating;
+    bool accelerating;
 
     private Rigidbody rb;
 
@@ -20,20 +27,61 @@ public class ShipMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
     }
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S)) accelerating = false;
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+        {
+            accelerating = true;
+            if (Input.GetKeyDown(KeyCode.W))_acceleration = acceleration;
+            else _acceleration = -acceleration;
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) rotAccelerating = false;
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            rotAccelerating = true;
+            if (Input.GetKeyDown(KeyCode.A)) _rotationRate = -rotationRate;
+            else _rotationRate = rotationRate;
+        }
+
+    }
     void FixedUpdate()
     {
-        z = 0;
-        if (Input.GetKey(KeyCode.W)) z = acceleration;
-        if (Input.GetKey(KeyCode.S)) z = -acceleration;
-        
-        if (Input.GetKey(KeyCode.A) && x > -maxRotation) x += -rotationRate;
-        if (Input.GetKey(KeyCode.D) && x < maxRotation) x += rotationRate;
-
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            if (z < velocity) z += _acceleration;
+            else z = velocity;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (z > -velocity) z += _acceleration;
+            else z = -velocity;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (x > -maxRotation) x += _rotationRate;
+            else x = -maxRotation;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (x < maxRotation) x += _rotationRate;
+            else x = maxRotation;
+        }
+        if (!accelerating)
+        {
+            if (Mathf.Abs(z) > 0.5) z *= decceleration;
+            else z = 0;           
+        }
+        if(!rotAccelerating)
+        {
+            if(Mathf.Abs(x) > 0.05) x *= rotationDecceleration;
+            else x = 0;
+        }
         direction = transform.forward.normalized * z;
-        rotation = transform.up.normalized * x;
+        rotation = Vector3.up.normalized * x;
         transform.Rotate(rotation);
         //print(rb.velocity.magnitude + " " + maxVelocity);
         //if (rb.velocity.magnitude < maxVelocity)
-            rb.velocity += direction;
+        rb.velocity = direction;
     }
 }
