137a2ddf68893d198f7e2ab3c49e5a184b1b7971
Phyrol <nitehawkk123@gmail.com>
2021-02-09 22:14:15 -0700

    audio. scene name. script added to ship

--- Assets/Scripts/Ship/Honk.cs
+++ Assets/Scripts/Ship/Honk.cs
@@ -10,18 +10,17 @@ public class Honk : MonoBehaviour
         {
             int chance = Random.Range(0, 100);
             AudioManager temp = AudioManager.instance;
-            if (chance > 0)
+            if (!temp.manualVolumeSources[0].isPlaying && !temp.manualVolumeSources[1].isPlaying)
             {
-                int rand = Random.Range(0, 2);
-                
-                if (!temp.manualVolumeSources[0].isPlaying && !temp.manualVolumeSources[1].isPlaying)
+                if (chance > 0)
                 {
+                    int rand = Random.Range(0, 2);
                     temp.manualVolumeSources[rand].Play();
                 }
-            }
-            else
-            {
-                temp.manualVolumeSources[8].Play();
+                else
+                {
+                    temp.manualVolumeSources[8].Play();
+                }
             }
             
         }
